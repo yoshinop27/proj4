b36c4f18af0e6da18819b82c3fbd1a1d05cdad9a
Preston Yoshino <prestonyoshino@gmail.com>
2026-04-25 00:23:08 -0500

    implement dfs of our mst to build tsp cycle within optimal range

--- tsp.cpp
+++ tsp.cpp
@@ -9,6 +9,7 @@
  */
 
 #include "tsp.hpp"
+#include <stack>
 
 using namespace std;
 
@@ -21,7 +22,24 @@ vector<int> tsp(vector<Vertex> &adjList, Vertex &start) {
     
     // Create the vector for tracking visited during the DFS.
     int n = adjList.size();
-    vector<bool> visited(n, false);
+    vector<bool> visited(n, false); 
+    // perform DFS
+    stack<Vertex> st;
+    st.push(start);
+    visited[start.label] = true;
+    while (!st.empty()) {
+        Vertex cur = st.top();
+        st.pop();
+        for (int i=0; i<cur.mstNeighbors.size(); i++){
+            int neighborIdx = cur.mstNeighbors[i];
+            if (!visited[neighborIdx]){
+                st.push(adjList[neighborIdx]);
+                visited[neighborIdx] = true;
+            }
+        }
+        tour.push_back(cur.label);
+    }
+    tour.push_back(start.label);
 
     return tour;
 }
