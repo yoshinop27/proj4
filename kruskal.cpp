efb236f257ffd69fa644556aedc189cc78a85da6
Preston Yoshino <prestonyoshino@gmail.com>
2026-04-24 22:03:19 -0500

    too many outer loops, not adding mst neightbors, incorrect intiial value)

--- kruskal.cpp
+++ kruskal.cpp
@@ -9,6 +9,7 @@
  */
 
 #include "kruskal.hpp"
+#include "algorithm"
 
 using namespace std;
 
@@ -57,22 +58,28 @@ vector<Edge> kruskal(vector<Vertex> &adjList, vector<Edge> &edgeList) {
     int n = adjList.size();
     vector<int> rank(n, 0);
     vector<int> pi(n);
-    int position = 0;
+    // fill in parent array with vertex labels
     for (int i=0; i<pi.size(); i++) {
         pi[i] = adjList[i].label;
     }
-    for(int j = 0; j < edgeList.size(); j++)
-    {
-    Edge minEdge = edgeList[0];
-    for(int i = 0; i < edgeList.size(); i++) {
-        if((edgeList[i].weight < minEdge.weight) && (find(edgeList[i].first.label, pi) != find(edgeList[i].second.label, pi))) {
-            minEdge.weight = edgeList[i].weight;
-            position = i;
+    // Find the minimum edge that's not contained in the same component
+    for(int j = 0; j < pi.size()-1; j++) {
+        double minWeight = INFINITY;
+        int minIdx = -1;    
+        // Search all edges
+        for(int i = 0; i < edgeList.size(); i++) {
+            if(edgeList[i].weight < minWeight && (find(edgeList[i].first.label, pi) != find(edgeList[i].second.label, pi))) {
+                minIdx = i;
+                minWeight = edgeList[i].weight;
+            }
         }
-    }
-    
-    union_by_rank(minEdge.first.label, minEdge.second.label, rank, pi);
-    mst.push_back(minEdge);
+        Edge minEdge = edgeList[minIdx];
+        // Union components
+        union_by_rank(minEdge.first.label, minEdge.second.label, rank, pi);
+        mst.push_back(minEdge);
+        // Add to mstNeighbors
+        adjList[minEdge.first.label].mstNeighbors.push_back(minEdge.second.label);
+        adjList[minEdge.second.label].mstNeighbors.push_back(minEdge.first.label);
     }
 
     return mst;
